938423443ad649080f93f7904f00316684a633db
geppetto <ad105geppetto@gmail.com>
2022-04-26 06:20:10 +0900

    classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+// 인터페이스를 쓴다면 ts에서는 좀더 안전하다.
+// 그러나 리액트나 노드, 익스프레스 등을 사용한다면 class가 필요할 수 있다.
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name:string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "Nicolas",
-    age: 22,
-    gender: "male",
-}
+const lynn = new Human('lynn', 20, 'female')
 
 const sayHi = (person:Human): string => {
     return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`)
 }
 
-console.log(sayHi(person))
+console.log(sayHi(lynn))
 export {};
