a8d2b9e263952db43989c919a003ea71806ac8cd
geppetto <ad105geppetto@gmail.com>
2022-04-27 19:12:11 +0900

    Blockchain creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 // 인터페이스를 쓴다면 ts에서는 좀더 안전하다.
 // 그러나 리액트나 노드, 익스프레스 등을 사용한다면 class가 필요할 수 있다.
 
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name:string, age:number, gender:string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number){
+        this.hash = hash;
+        this.index = index;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const lynn = new Human('lynn', 20, 'female')
+const genesisBlock:Block = new Block(0, "20202020202", "", "Hello", 123456);
 
-const sayHi = (person:Human): string => {
-    return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`)
-}
-
-console.log(sayHi(lynn))
+let blockchain: [Block] = [genesisBlock]
+console.log(genesisBlock)
 export {};
